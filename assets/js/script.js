112798d713aea0770b9804c69ffe6ad1239eed11
ravib7 <ravibchaudhari7@gmail.com>
2024-11-19 15:05:12 +0530

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,10 @@
+
+// Navbar Start
+document.getElementById("logo").addEventListener("click", function (event) {
+    event.preventDefault();
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+});
+//Navbar End
